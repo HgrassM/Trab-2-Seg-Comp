ba23295b0f4cffc38fa72df67d74ccdf53d35c85
Heitor <heitordemattosolima@gmail.com>
2025-09-30 11:03:29 -0300

    Correção e otimização na geração de números primos

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-from keygen import genprimes
+from rsa import genprimes
 
-print(f"Generation prime number 2048 bits long:\n {genprimes.genPrimeNumber(2048)}")
+print(f"Generating prime number that is 2048 bits long:\n{genprimes.genPrimeNumber(2048)}")
 
-number = 8683317618811886495518194401279999999
+number = 359334085968622831041960188598043661065388726959079837
 
 print(f"\n\nThe number {number} is prime? {genprimes.millerRabin(number, 40)}")
--- src/rsa/genprimes.py
+++ src/rsa/genprimes.py
@@ -34,7 +34,7 @@ def lowPrimeTest(prime_number_candidate, matrix_size):
 
     #Divide o cadidato a número primo pelos primos menores que ele
     for prime in p_list:
-        if (prime_number_candidate % prime == 0) and (prime**2 <= prime_number_candidate):
+        if (prime_number_candidate % prime == 0):
             return False
 
     return True
